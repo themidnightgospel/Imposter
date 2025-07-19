b655ab9da8d0f1ec1920d35ac49b953cd1511760
Bitchiko Tchelidze <bubachelidze1@gmail.com>
2025-07-19 23:50:40 +0400

    Before property setup.

---
+++ Imposter/VerificationFailedException.cs
@@ -0,0 +1,10 @@
+﻿namespace Imposter;
+
+public class VerificationFailedException : Exception
+{
+    public VerificationFailedException(string message) : base(message)
+    { }
+
+    public VerificationFailedException(string message, Exception innerException) : base(message, innerException)
+    { }
+}
