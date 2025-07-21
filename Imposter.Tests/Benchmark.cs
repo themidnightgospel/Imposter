a35b44129c92027b5fab3f3eb46644c3124cd261
Bitchiko Tchelidze <bubachelidze1@gmail.com>
2025-07-21 12:10:31 +0400

    Added generato class

--- Imposter.Tests/Benchmark.cs
+++ Imposter.Tests/Benchmark.cs
@@ -3,6 +3,7 @@ using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 using Imposter;
 using Moq;
+using NSubstitute;
 using Shouldly;
 
 var mock = new CalculatorImposter();
@@ -23,12 +24,12 @@ var mock = new CalculatorImposter();
 // var res = mock.Add(8, 2);
 // Console.WriteLine(res);
 
-var calculatorMock = new Mock<ICalculator>();
-calculatorMock
-    .Setup(it => it.Add(It.IsAny<int>(), It.IsAny<int>()))
-    .Returns(1);
+// var calculatorMock = new Mock<ICalculator>();
+// calculatorMock
+//     .Setup(it => it.Add(It.IsAny<int>(), It.IsAny<int>()))
+//     .Returns(1);
 
-calculatorMock.Verify(it => it.Add(It.IsAny<int>(), It.IsAny<int>()), Times.Once);
+// calculatorMock.Verify(it => it.Add(It.IsAny<int>(), It.IsAny<int>()), Times.Once);
 
 BenchmarkRunner.Run<Benchy>();
 
@@ -49,12 +50,22 @@ public class Benchy
     }
 
     [Benchmark]
+    public void NSub()
+    {
+        var calculatorMock = Substitute.For<ICalculator>();
+
+        calculatorMock.Add(Arg.Any<int>(), Arg.Any<int>()).Returns(1);
+
+        calculatorMock.Add(1, 1);
+    }
+
+    [Benchmark]
     public void Imposter()
     {
         var imposter = new CalculatorImposter();
 
         imposter
-            .Add(ParameterCriteria<int>.IsAny(), ParameterCriteria<int>.IsAny())
+            .Add(Arg<int>.IsAny(), Arg<int>.IsAny())
             .Returns((left, right) => left * right);
 
         imposter.ImposterInstance().Add(10, 9).ShouldBe(10 * 9);
