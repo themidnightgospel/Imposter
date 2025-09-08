5b65508f9f8899942df6201c4f37a2e7d93bd93d
Bitchiko Tchelidze <Bitchiko.Tchelidze@mt.com>
2025-09-08 11:01:24 +0200

    Added Abstractions library to house shared types.

---
+++ Imposter.Abstractions/ImposterExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Imposter.Abstractions;
+
+/// <summary>
+/// TODO Add Doc
+/// </summary>
+public static class ImposterExtensions
+{
+    public static TVerifier Verify<TVerifier>(this IHaveImposterVerifier<TVerifier> imposter)
+        => imposter.Verify();
+
+    public static TInstance Instance<TInstance>(this IHaveImposterInstance<TInstance> imposter)
+        => imposter.Instance();
+}
