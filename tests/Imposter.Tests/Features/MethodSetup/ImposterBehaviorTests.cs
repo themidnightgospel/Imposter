6e1f136ab384a3d1269699ce809cd670c8d9c443
Bitchiko Tchelidze <Bitchiko.Tchelidze@mt.com>
2025-11-05 01:41:38 +0400

    Add untracked files.

---
+++ tests/Imposter.Tests/Features/MethodSetup/ImposterBehaviorTests.cs
@@ -0,0 +1,38 @@
+using Imposter.Abstractions;
+using Shouldly;
+using Xunit;
+
+namespace Imposter.CodeGenerator.Tests.Features.MethodSetup
+{
+    public class ImposterBehaviorTests
+    {
+        [Fact]
+        public void GivenExplicitMode_WhenMethodInvokedWithoutSetup_ShouldThrowMissingImposterException()
+        {
+            var sut = new IMethodSetupFeatureSutImposter(ImposterInvocationBehavior.Explicit);
+
+            var exception = Should.Throw<MissingImposterException>(() => sut.Instance().IntNoParams());
+            exception.MethodName.ShouldNotBeNull().ShouldContain(nameof(IMethodSetupFeatureSut.IntNoParams));
+        }
+
+        [Fact]
+        public void GivenExplicitMode_WhenSetupDoesNotConfigureResult_ShouldThrowMissingImposterException()
+        {
+            var sut = new IMethodSetupFeatureSutImposter(ImposterInvocationBehavior.Explicit);
+
+            sut.IntNoParams(); // Builder created but no Returns/Throws.
+
+            Should.Throw<MissingImposterException>(() => sut.Instance().IntNoParams());
+        }
+
+        [Fact]
+        public void GivenExplicitMode_WhenSetupConfigured_ShouldInvokeSuccessfully()
+        {
+            var sut = new IMethodSetupFeatureSutImposter(ImposterInvocationBehavior.Explicit);
+
+            sut.IntNoParams().Returns(123);
+
+            sut.Instance().IntNoParams().ShouldBe(123);
+        }
+    }
+}
---
+++ tests/Imposter.Tests/Features/PropertySetup/PropertyBehaviorTests.cs
@@ -0,0 +1,46 @@
+using Imposter.Abstractions;
+using Shouldly;
+using Xunit;
+
+namespace Imposter.CodeGenerator.Tests.Features.PropertySetup
+{
+    public class PropertyBehaviorTests
+    {
+        [Fact]
+        public void GivenExplicitMode_WhenGetterInvokedWithoutSetup_ShouldThrowMissingImposterException()
+        {
+            var sut = new IPropertySetupSutImposter(ImposterInvocationBehavior.Explicit);
+
+            var exception = Should.Throw<MissingImposterException>(() => sut.Instance().Age);
+            exception.MethodName.ShouldNotBeNull().ShouldContain(nameof(IPropertySetupSut.Age));
+        }
+
+        [Fact]
+        public void GivenExplicitMode_WhenGetterConfigured_ShouldReturnValue()
+        {
+            var sut = new IPropertySetupSutImposter(ImposterInvocationBehavior.Explicit);
+
+            sut.Age.Getter().Returns(42);
+
+            sut.Instance().Age.ShouldBe(42);
+        }
+
+        [Fact]
+        public void GivenExplicitMode_WhenSetterInvokedWithoutSetup_ShouldThrowMissingImposterException()
+        {
+            var sut = new IPropertySetupSutImposter(ImposterInvocationBehavior.Explicit);
+
+            Should.Throw<MissingImposterException>(() => sut.Instance().LastName = 5);
+        }
+
+        [Fact]
+        public void GivenExplicitMode_WhenSetterConfigured_ShouldAllowSettingValues()
+        {
+            var sut = new IPropertySetupSutImposter(ImposterInvocationBehavior.Explicit);
+
+            sut.LastName.Setter(Arg<int>.Any());
+
+            Should.NotThrow(() => sut.Instance().LastName = 5);
+        }
+    }
+}
