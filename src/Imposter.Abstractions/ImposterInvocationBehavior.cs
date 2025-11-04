42af7d6e9162069e404c8c998f480efe8d3d26f4
Bitchiko Tchelidze <bubachelidze1@gmail.com>
2025-11-05 01:41:38 +0400

    Add untracked files.

---
+++ src/Imposter.Abstractions/ImposterInvocationBehavior.cs
@@ -0,0 +1,17 @@
+namespace Imposter.Abstractions;
+
+/// <summary>
+/// Specifies how an imposter should behave when a member is invoked without an explicit setup.
+/// </summary>
+public enum ImposterInvocationBehavior
+{
+    /// <summary>
+    /// Implicit (loose) mode – missing setups fall back to default return values.
+    /// </summary>
+    Implicit = 0,
+
+    /// <summary>
+    /// Explicit (strict) mode – missing setups throw <see cref="MissingImposterException"/>.
+    /// </summary>
+    Explicit = 1
+}
---
+++ src/Imposter.Abstractions/MissingImposterException.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace Imposter.Abstractions;
+
+/// <summary>
+/// Exception that is thrown when an imposter running in <see cref="ImposterInvocationBehavior.Explicit"/> mode
+/// receives an invocation that has not been configured.
+/// </summary>
+[Serializable]
+public class MissingImposterException : Exception
+{
+    /// <summary>
+    /// Gets the fully-qualified method or member name associated with the missing setup.
+    /// </summary>
+    public string? MethodName { get; }
+
+    /// <inheritdoc />
+    public MissingImposterException()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MissingImposterException"/> class for the specified method.
+    /// </summary>
+    public MissingImposterException(string methodName)
+        : base($"No imposter setup was configured for '{methodName}'.")
+    {
+        MethodName = methodName;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MissingImposterException"/> class with an inner exception.
+    /// </summary>
+    public MissingImposterException(string methodName, Exception innerException)
+        : base($"No imposter setup was configured for '{methodName}'.", innerException)
+    {
+        MethodName = methodName;
+    }
+
+    /// <inheritdoc />
+    protected MissingImposterException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
+    {
+        MethodName = info.GetString(nameof(MethodName));
+    }
+
+    /// <inheritdoc />
+    public override void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        base.GetObjectData(info, context);
+        info.AddValue(nameof(MethodName), MethodName);
+    }
+}
