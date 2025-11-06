a8a43b09250494f31dd3f86e5f2ba67c92aef8a6
Bitchiko Tchelidze <bubachelidze1@gmail.com>
2025-11-06 10:47:19 +0400

    Add thread safety tests

---
+++ tests/Imposter.Tests/Features/MethodImposter/ThreadSafetyTests.cs
@@ -0,0 +1,73 @@
+using System.Threading;
+using Imposter.Abstractions;
+using Shouldly;
+using Xunit;
+
+namespace Imposter.CodeGenerator.Tests.Features.MethodImposter
+{
+    public class ThreadSafetyTests
+    {
+        private const int ThreadCount = 100;
+        private readonly IMethodSetupFeatureSutImposter _sut = new IMethodSetupFeatureSutImposter();
+
+        [Fact]
+        public void GivenConcurrentOperations_WhenMixingVoidAndReturnMethods_ShouldNotThrowExceptions()
+        {
+            var threads = new Thread[ThreadCount];
+
+            for (int i = 0; i < ThreadCount; i++)
+            {
+                var index = i;
+                threads[i] = index % 2 == 0
+                    ? new Thread(() => _sut.Instance().IntNoParams())
+                    : new Thread(() => _sut.Instance().VoidNoParams());
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            Should.NotThrow(() => _sut.IntNoParams().Called(Count.AtLeast(ThreadCount / 2)));
+            Should.NotThrow(() => _sut.VoidNoParams().Called(Count.AtLeast(ThreadCount / 2)));
+        }
+
+        [Fact]
+        public void GivenSequentialReturnsSetup_WhenMethodsInvokedConcurrently_ShouldConsumeUniqueValues()
+        {
+            var builder = _sut.IntNoParams();
+            builder.Returns(0);
+            for (int i = 1; i < ThreadCount; i++)
+            {
+                builder.Then().Returns(i);
+            }
+
+            var results = new int[ThreadCount];
+            var threads = new Thread[ThreadCount];
+
+            for (int i = 0; i < ThreadCount; i++)
+            {
+                var index = i;
+                threads[i] = new Thread(() => results[index] = _sut.Instance().IntNoParams());
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+
+            results.ShouldBeUnique();
+            results.ShouldAllBe(x => x >= 0 && x < ThreadCount);
+        }
+    }
+}
